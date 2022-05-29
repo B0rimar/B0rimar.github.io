2086ad9bd286354054e92977d51ae5405899768a
B0rimar <104488767+B0rimar@users.noreply.github.com>
2022-05-29 22:37:44 +0700

    Дз заданное после 10 урока

---
+++ src/js/myScript.js
@@ -0,0 +1,34 @@
+"use script"
+
+var nameUser=prompt("Здравствуйте, какой сайт Вам необходим?", "Интернет-магазин");
+  
+    alert( nameUser + ". Сделаем! ")
+
+/*function firstName (){
+
+
+  let firstName = prompt("Как Вас зовут?");
+
+  if(firstName)
+
+  {
+
+    alert("Добро пожаловать " + firstName);
+
+  }
+
+  else
+
+  {
+
+    alert("Вы нажали отменить");
+
+  }
+}*/
+var nameUser=prompt("Желаемый дизайн?", "Минимализм");
+
+    alert(nameUser + " - Легко! ");
+var nameUser=prompt("Необходимая вёрстка?", "Адаптивная");
+
+    alert(nameUser + ". Отличный выбор!");
+ alert("Общая стоимость: 15000 руб.");
