8c00d35a1285219266b578ad84cdb83bc5fa5c4d
B0rimar <104488767+B0rimar@users.noreply.github.com>
2022-06-07 19:06:54 +0700

    Дз после 12 урока

--- src/js/myScript.js
+++ src/js/myScript.js
@@ -1,7 +1,7 @@
 "use script"
 
 
-var a = parseInt(prompt ("Какой сайт Вам необходим?:  1 - Сайт-Визитка, 2 - Интернет-магазин, 3 - Корпоратиный сайт"));
+/*var a = parseInt(prompt ("Какой сайт Вам необходим?:  1 - Сайт-Визитка, 2 - Интернет-магазин, 3 - Корпоратиный сайт"));
 if (a == "1"){
 //выполняешь расчет
 } else if (a == "2"){
@@ -32,5 +32,69 @@ alert("Вероятно, Вы ошиблись");
 }
 var summa = a + b + c;  
 alert("Сумма = " + summa * 2000);
+*/
 
-//let  design2 design
+
+$(document).ready(function() {
+
+	$('.image-popup-vertical-fit').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		mainClass: 'mfp-img-mobile',
+		image: {
+			verticalFit: true
+		}
+		
+	});
+
+	$('.image-popup-fit-width').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		image: {
+			verticalFit: false
+		}
+	});
+
+	$('.image-popup-no-margins').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		closeBtnInside: false,
+		fixedContentPos: true,
+		mainClass: 'mfp-no-margins mfp-with-zoom', 
+		image: {
+			verticalFit: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300 
+		}
+	});
+
+});
+
+$("body").on('click', '[href*="#"]', function(e){
+  var fixed_offset = 100;
+  $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 500);
+  e.preventDefault();
+});
+
+
+
+
+
+$(document).ready (function(){
+    let options = {threshold:[0,5]};
+    let observer = new IntersectionObserver(onEntry,options);
+    let elements = $(".element-animation");
+    elements.each (i,el)=>{
+        observer.observe(el);
+    }
+});
+function onEntry (entry){
+    entry.forEach(change=>{
+        if (change.isIntersecting){
+            change.target.src = change.target.dataset.src;
+        }
+        
+    });
+}
